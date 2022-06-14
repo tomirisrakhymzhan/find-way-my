464326a983fba2b091cfc6b1c9a5c3641206d9b2
tomiris.rakhymzhan <tomiris.rakhymzhan@gmail.com>
2022-06-14 16:49:04 +0100

    Updates

---
+++ precache-manifest.603205af1b9a416c09a673f8c23737be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac721df43ecb795b068f",
+    "url": "/find-way-my/static/css/main.2c215719.chunk.css"
+  },
+  {
+    "revision": "ac721df43ecb795b068f",
+    "url": "/find-way-my/static/js/main.ac721df4.chunk.js"
+  },
+  {
+    "revision": "8bdf6a416b3de2f35de0",
+    "url": "/find-way-my/static/css/1.443f7d14.chunk.css"
+  },
+  {
+    "revision": "8bdf6a416b3de2f35de0",
+    "url": "/find-way-my/static/js/1.8bdf6a41.chunk.js"
+  },
+  {
+    "revision": "d2101c290e245ae47aca",
+    "url": "/find-way-my/static/js/runtime~main.d2101c29.js"
+  },
+  {
+    "revision": "261f02224675ebd9e5721692f0a9892c",
+    "url": "/find-way-my/index.html"
+  }
+];
